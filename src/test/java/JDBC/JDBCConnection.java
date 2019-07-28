aeefbba50e051a87b7226df0901e40db529f3a0b
AihemaitijiangYusufu <yuraphd@gmail.com>
2019-07-28 08:47:17 -0400

    jenkins test

---
+++ src/test/java/JDBC/JDBCConnection.java
@@ -0,0 +1,17 @@
+package JDBC;
+
+import org.testng.annotations.Test;
+
+public class JDBCConnection {
+
+
+//    String oracledbUrl=
+//    String  oracledbUsername=
+//    String oraclePassword=
+
+    @Test
+    public void oracleJDBC(){
+
+
+    }
+}
