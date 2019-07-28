df71e7463c66f42836ad9e6243896a2a4aa760c3
AihemaitijiangYusufu <yuraphd@gmail.com>
2019-07-28 11:58:17 -0400

    postgres

--- src/test/java/JDBC/JDBCConnection.java
+++ src/test/java/JDBC/JDBCConnection.java
@@ -2,16 +2,30 @@ package JDBC;
 
 import org.testng.annotations.Test;
 
+import java.sql.*;
+
 public class JDBCConnection {
 
 
-//    String oracledbUrl=
-//    String  oracledbUsername=
-//    String oraclePassword=
+    String  oracledbUrl="jdbc:postgresql://room-reservation-qa.cxvqfpt4mc2y.us-east-1.rds.amazonaws.com:5432/hr";
+    String  oracledbUsername="hr";
+    String  oraclePassword="hr";
 
     @Test
-    public void oracleJDBC(){
+    public void oracleJDBC() throws SQLException, ClassNotFoundException {
 
+
+       Class.forName("org.postgresql.Driver");
+        Connection connection= DriverManager.getConnection(oracledbUrl,oracledbUsername,oraclePassword);
+        Statement statement=connection.createStatement();
+        ResultSet resultSet=statement.executeQuery("select * from countries");
+
+        resultSet.next();
+        System.out.println(resultSet.getString("country_name"));
+
+        resultSet.close();
+        statement.close();
+        connection.close();
 
     }
 }
